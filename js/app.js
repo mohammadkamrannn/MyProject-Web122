42c8d0651a86ddb31aaa9476b3b7f56d3dd06b81
Mohammad Kamran <mohammadkamrannn@gmail.com>
2021-12-18 23:58:16 +0330

    Add owl carousel and other content

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
 $(document).ready(function(){
-    $('.sidebar ul.navbar-nav a').click(function(){
-        $('.sidebar ul.navbar-nav a').removeClass('current');
-        $(this).addClass('current');
-        event.preventDefault();
-    });
+    $('.owl-carousel').owlCarousel({
+        rtl:true,
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+
+
+
+
+
+    // $('.sidebar ul.navbar-nav a').click(function(){
+    //     $('.sidebar ul.navbar-nav a').removeClass('current');
+    //     $(this).addClass('current');
+    //     event.preventDefault();
+    // });
+
 });
