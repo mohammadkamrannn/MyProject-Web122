6331c661dfebaab134ce97b31794613baa78d796
Mohammad Kamran <mohammadkamrannn@gmail.com>
2021-12-27 12:37:36 +0330

    Final - Responsive - Add Autoplay owlCarousel

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ $(document).ready(function(){
         loop:true,
         margin:10,
         nav:false,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
         responsive:{
             0:{
                 items:1
