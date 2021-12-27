83245cdc68d6109046926c7d6a4a581562d97f64
Mohammad Kamran <mohammadkamrannn@gmail.com>
2021-12-27 12:34:11 +0330

    Final - Responsive - Remove Nav Brands

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         rtl:true,
         loop:true,
         margin:10,
-        nav:true,
+        nav:false,
         responsive:{
             0:{
                 items:1
@@ -16,15 +16,5 @@ $(document).ready(function(){
             }
         }
     })
-
-
-
-
-
-    // $('.sidebar ul.navbar-nav a').click(function(){
-    //     $('.sidebar ul.navbar-nav a').removeClass('current');
-    //     $(this).addClass('current');
-    //     event.preventDefault();
-    // });
 
 });
